4839e06ae7e95c25035d0cc8e736b49854f2f682
sajibkhandev <sajibkhan.mern@gmail.com>
2025-04-17 19:19:44 +0600

    add react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,42 @@
 import React from 'react'
+import {
+  createRoutesFromElements,
+  createBrowserRouter,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './pages/Home';
+import Shop from './pages/Shop';
+import Product from './pages/Product';
+import Contact from './pages/Contact';
+import About from './pages/About';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Cart from './pages/Cart';
+import Checkout from './pages/Checkout';
+import Error from './pages/Error';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+    <Route path="/" element={<Home />}></Route>
+    <Route path="/shop" element={<Shop />}></Route>
+    <Route path="/product" element={<Product />}></Route>
+    <Route path="/contact" element={<Contact />}></Route>
+    <Route path="/about" element={<About />}></Route>
+    <Route path="/login" element={<Login />}></Route>
+    <Route path="/signup" element={<Signup />}></Route>
+    <Route path="/cart" element={<Cart />}></Route>
+    <Route path="/checkout" element={<Checkout />}></Route>
+    <Route path="/*" element={<Error />}></Route>
+    </>
+  )
+);
+
 
 const App = () => {
   return (
-    <h1 className='bg-red-500'>Thss is taiwlind css</h1>
+    <RouterProvider router={router} />
   )
 }
 
