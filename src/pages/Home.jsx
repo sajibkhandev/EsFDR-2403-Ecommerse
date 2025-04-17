121c10afa0eec84554520d9064d8e163d5fa80b0
sajibkhandev <sajibkhan.mern@gmail.com>
2025-04-17 19:48:38 +0600

    all componentdone

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import Container from '../components/Container'
+import Flex from '../components/Flex'
+import Image from '../components/Image'
+import Cart from '../assets/cart.jpg'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+       <h1 className='bg-sajib text-white'>thi si ssajib khan</h1>
+    </div>
   )
 }
 
